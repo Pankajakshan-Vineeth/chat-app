5c765741e57b8ce1ff5a7aba5980d01c7dacf760
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-10 22:03:51 +0530

    profile page half completed

--- src/Pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/Pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import avatar_icon from '../../assets/avatar_icon.png'
+import logo_icon from '../../assets/logo_icon.png'
 
 const ProfileUpdate = () => {
   return (
-    <div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+          <input type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden />
+          <img src={avatar_icon} alt="" />
+          Upload profile image
+          </label>
+          <input type="text" placeholder='your name' required/>
+          <textarea placeholder='Write profile bio' required></textarea>
+          <button type ='submit'>Submit</button>
+        </form>
+        <img className='profile-pic' src={logo_icon} alt="" />
+      </div>
     </div>
   )
 }
